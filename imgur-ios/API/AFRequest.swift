65a0013daf4dfd163a49e59abfb1cd3f9b54f1aa
rodrigo.tomiosso <rodrigobt20@gmail.com>
2020-09-17 16:23:52 -0300

    [galleryApiRequest] Created API request module using Alamofire

---
+++ imgur-ios/API/AFRequest.swift
@@ -0,0 +1,21 @@
+import Foundation
+import Alamofire
+
+class AFRequest: APIRequest {
+
+    func get(url: URL, onSuccess: @escaping APIResponseSuccess, onError: @escaping ErrorClosure) {
+        print("Request to \(url)")
+
+        AF.request(url).responseData { (response) in
+                if let error = response.error {
+                    onError(error)
+                    return
+                }
+                guard let data = response.value else {
+                    onError(CustomError.serverError(details: "Empty Response for \(url)"))
+                    return
+                }
+                onSuccess(data)
+        }
+    }
+}
